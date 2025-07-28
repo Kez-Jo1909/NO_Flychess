8b48bcbfb6934f6e89d7bff24338a2590549916e
Kez Jo <20235083@stu.neu.edu.cn>
2025-07-28 17:49:40 +0800

    [add]main menu

--- client/include/MainWindow.h
+++ client/include/MainWindow.h
@@ -2,10 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+namespace flychess_client{
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -15,10 +18,20 @@ public:
     ~MainWindow();
 
 private slots:
-    void onButtonClicked();  // 响应按钮槽函数
+    void StartButtonClicked();  // 开始按钮槽函数
 
+    void on_ExitButton_clicked();// 退出按钮槽函数
+
+private:
+    void repositionStartMenu();
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
+// 以下是私有成员变量
 private:
     Ui::MainWindow *ui;
 };
 
+}// namespace flychess_client
 #endif // MAINWINDOW_H
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    MainWindow window;
+    flychess_client::MainWindow window;
     window.show();
 
     return app.exec();
