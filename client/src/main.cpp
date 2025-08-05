8aa172b3daa664e7f3231a2514d2ed5de37a932b
Kez Jo <20235083@stu.neu.edu.cn>
2025-08-05 15:46:01 +0800

    [add]制定utf8

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,7 +1,14 @@
 #include <QApplication>
 #include "MainWindow.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main(int argc, char *argv[]) {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
     QApplication app(argc, argv);
 
     flychess_client::MainWindow window;
